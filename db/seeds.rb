cba4c73f4e28e7f5457017abcbd0b361b10d2b21
alkasoun123 <alkasoun.24@gmail.com>
2016-09-13 05:37:41 +0530

    remove seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.posts.create!(content: content) }
-end
